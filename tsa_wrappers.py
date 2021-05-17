1b7d0432c676d645333c3eb308693b105a6477c8
Adriane Amorado <adrianemikko@gmail.com>
2021-05-18 03:15:17 +0800

    Changed to scratch notebook

--- tsa_wrappers.py
+++ tsa_wrappers.py
@@ -1,16 +1,30 @@
 ######################################
 ##             Wrappers             ##
 ######################################
-import numpy as np
+from pandas.core.series import Series
 
 
-class TSABenchmarkModel:
-    def __init__(self, func, **kwargs):
+class BaseFuncModel:
+    def __init__(self, func: function, **kwargs) -> None:
         self.func = func
         self.kwargs = kwargs
 
-    def fit(self, ts):
+    def fit(self, ts: Series) -> BaseFuncModel:
         self.ts = ts
+        return self
 
-    def forecast(self, h):
+    def forecast(self, h: int) -> Series:
         return self.func(self.ts, h, **self.kwargs)
+
+
+class StatsModelsWrapper:
+    def __init__(self, model, **kwargs) -> None:
+        self.model = model
+        self.kwargs = kwargs
+
+    def fit(self, ts: Series) -> StatsModelsWrapper:
+        self.model.fit(ts, **self.kwargs)
+        return self
+
+    def forecast(self, h: int) -> Series:
+        return self.model.forecast(h)
